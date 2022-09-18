47eb07280452f93717149b3a5a1dcc6d1f71e4d6
Andrii-Nechiporuk1 <andr182002@gmail.com>
2022-09-18 21:43:35 +0300

    Add project files.

---
+++ Catalog/Models/CatalogDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Models
+{
+    public class CatalogDbContext
+    {
+        public CatalogDbContext(DbContextOptions<CatalogDbContext> options) :base(options)
+        {
+        
+        }
+    }
+}
